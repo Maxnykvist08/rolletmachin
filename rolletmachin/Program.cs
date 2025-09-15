c92a168632c98336ff9cc64d94160197b041994c
Maxnykvist08 <14MN0702@amal.se>
2025-09-15 11:00:58 +0200

    Add project files.

---
+++ rolletmachin/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("summa");
+string inputsumma = Console.ReadLine();
+int betsumma = Convert.ToInt32(inputsumma);
+List<string> betplay = ["numer" , "red or black", "even or odd", "1 to 18", "19 to 36", "1st 12"]
+
+Console.WriteLine(" mellan 0 och 36");
+string inputnumer = Console.ReadLine();
+int betnumber = Convert.ToInt32(inputnumer);
+
+
+Random random = new Random();
+
+
+int number = random.Next(36);
+
+Console.WriteLine(number);
+
+if (betnumber == number)
+{
+    Console.WriteLine("du van");
+}
+
+
+
