19583afe44d8cfa3618014c1f633cb12f1c00721
Maxnykvist08 <14MN0702@amal.se>
2025-09-17 11:28:44 +0200

    lister till talen, gjort så man kan köra igen börjat idotsäkra snyggat till consolen fått programet fungerande

--- rolletmachin/Program.cs
+++ rolletmachin/Program.cs
@@ -1,24 +1,196 @@
 ﻿
-Console.WriteLine("summa");
+
+Console.Write("put in totel amount =");
+bool vaidnumber = false;
 string inputsumma = Console.ReadLine();
-int betsumma = Convert.ToInt32(inputsumma);
-List<string> betplay = ["numer" , "red or black", "even or odd", "1 to 18", "19 to 36", "1st 12"]
-
-Console.WriteLine(" mellan 0 och 36");
-string inputnumer = Console.ReadLine();
-int betnumber = Convert.ToInt32(inputnumer);
-
+int summa = Convert.ToInt32(inputsumma);
 
 Random random = new Random();
 
+bool playagain = false;
+while(playagain == false) 
+{ 
 
+    Console.Clear();
 int number = random.Next(36);
+int loss = 0;
 
-Console.WriteLine(number);
 
-if (betnumber == number)
+Console.Write("totel bet =");
+
+string inputbetsumma ;
+    int betsumma = 0;
+    while (!vaidnumber)
+    {
+        inputbetsumma = Console.ReadLine();
+        betsumma = Convert.ToInt32(inputbetsumma);
+        if (betsumma <= summa)
+        {
+            vaidnumber = true;
+        }
+        else { Console.WriteLine("unvalid bet"); }
+    }
+
+Console.Clear();
+List<string> betplay = [ "numer", "red","black", "even"," odd", "1 to 18", "19 to 36", "1st 12", "2nd 12", "3rd 12"];
+
+for (int i = 0; i < betplay.Count; i++)
 {
-    Console.WriteLine("du van");
+    Console.WriteLine(i +1 + ":" + betplay[i]);
+
+
+}
+
+Console.WriteLine("input corosponding number to play style");
+    string playinput;
+    int playnumber = 0;
+    vaidnumber = false;
+    while (!vaidnumber)
+    {
+        playinput = Console.ReadLine();
+        playnumber = Convert.ToInt32(playinput);
+        if (playnumber >=1 && playnumber <= 10)
+        {
+            vaidnumber = true;
+        }else { Console.WriteLine("unvalid number"); }
+    }
+Console.Clear();
+int betnumber;
+string inputnumer;
+
+List <int> contener = new List<int>();
+int winnings = 0;
+
+if (playnumber == 1)
+{
+    Console.WriteLine(" mellan 0 och 36");
+    inputnumer = Console.ReadLine();
+    betnumber = Convert.ToInt32(inputnumer);
+    contener.Add(betnumber);
+    winnings = betsumma * 36;
+    if (betnumber == 0)
+        {
+            winnings = betsumma + loss *36;
+        }
+    
+
+}else if (playnumber == 2)
+{
+    contener = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+    winnings = betsumma * 2;
+}
+else if (playnumber == 3)
+{
+    contener = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+    winnings = betsumma * 2;
+}
+
+else if (playnumber == 4)
+{
+    for (int i = 0;i < 36; i++)
+    {
+        if (i % 2 == 0)
+        {
+            contener.Add(i);
+        }
+    }
+        winnings = betsumma * 2;
+}
+else if (playnumber == 5)
+{
+    for (int i = 0; i < 36; i++)
+    {
+        if (i % 2 == 1)
+        {
+            contener.Add(i);
+        }
+    }
+    winnings = betsumma * 2;
+}
+else if (playnumber == 6)
+{
+    for (int i = 0; i < 18; i++)
+    {
+        contener.Add(i);
+    }
+    winnings = betsumma * 2;
+}
+else if (playnumber == 7)
+{
+    for (int i = 18; i < 36; i++)
+    {
+        contener.Add(i);
+    }
+    winnings = betsumma * 2;
+}
+else if (playnumber == 8)
+{
+    for (int i = 0; i < 12; i++)
+    {
+        contener.Add(i);
+    }
+    winnings = betsumma * 2;
+}
+else if (playnumber == 9)
+{
+    for (int i = 13; i < 24; i++)
+    {
+        contener.Add(i);
+    }
+    winnings = betsumma * 2;
+}
+else if (playnumber == 10)
+{
+    for (int i = 25; i < 36; i++)
+    {
+        contener.Add(i);
+    }
+    winnings = betsumma * 2;
+}
+    Console.WriteLine(number);
+
+    if (contener.Contains(number))
+{
+    Console.WriteLine(betplay[playnumber -1 ] + " " + "won");
+    Console.WriteLine(winnings + "$");
+    Console.WriteLine("totel winings ="+( summa + winnings) + "$");
+    summa = summa + winnings;
+    }
+else
+{
+    Console.WriteLine("house won");
+        Console.WriteLine("totel loss =" + (summa - betsumma) + "$");
+        summa = summa - betsumma;
+        loss = loss + betsumma;
+    }
+    if (summa <= 0)
+    {
+        Console.WriteLine("out of money, do you want to put in more? yes or no");
+        
+        string outofmoney = Console.ReadLine();
+        if (outofmoney == "yes")
+        {
+            Console.Write("put in totel amount =");
+             inputsumma = Console.ReadLine();
+             summa = Convert.ToInt32(inputsumma);
+        }
+        else if (outofmoney == "no")
+        {
+          System.Environment.Exit(0);
+        }
+    }
+    Console.WriteLine("do you want to play again, yes or no");
+    string yesorno = Console.ReadLine();
+    if (yesorno == "yes ")
+    {
+        playagain = false;
+    }
+    else if (yesorno == "no")
+    {
+        playagain = true;
+    }
+
+
 }
 
 
